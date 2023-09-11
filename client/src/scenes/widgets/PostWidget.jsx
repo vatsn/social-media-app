121b1a47e603341159e51c4b74fdc8c7764fc252
Vatsal <vatsalnautiyal01@gmail.com>
2023-09-12 01:27:10 +0530

    initial commit 0

--- client/src/scenes/widgets/PostWidget.jsx
+++ client/src/scenes/widgets/PostWidget.jsx
@@ -64,7 +64,7 @@ import {
             height="auto"
             alt="post"
             style={{ borderRadius: "0.75rem", marginTop: "0.75rem" }}
-            src={`http://localhost:3001/assets/${picturePath}`}
+            src={`${process.env.REACT_APP_BACKEND_URL}/${picturePath}`}
           />
         )}
         <FlexBetween mt="0.25rem">
